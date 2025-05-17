38daa383e5a599f7d72a350c5ee7a547424a209b
GustavoOspinaL <tavolopez.dev@gmail.com>
2025-05-17 17:01:45 -0500

    feat: add title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h1>¡Universidad Católica de Pereira !</h1>
       <p>Listado de integrantes - Proceso de Desarrollo de Software I</p>
+      <h1>Estudiante: <tu nombre></h1>
     </div>
   );
 }
