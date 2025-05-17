5d77bae795ce68d497c7aef97593f8260e5ea727
GustavoOspinaL <tavolopez.dev@gmail.com>
2025-05-17 17:04:22 -0500

    feat update student name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1>¡Universidad Católica de Pereira !</h1>
       <p>Listado de integrantes - Proceso de Desarrollo de Software I</p>
-      <h1>Estudiante: <tu nombre></h1>
+      <h2>Estudiante: Gustavo Ospina</h2>
     </div>
   );
 }
